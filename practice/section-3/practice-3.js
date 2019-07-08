718662bf2de00e2e77c6b3ae6e2f45e3a2fa060b
曾志威 <51374393+DannyZeng2@users.noreply.github.com>
2019-07-08 23:48:26 +0800

    Update practice-3.js

--- practice/section-3/practice-3.js
+++ practice/section-3/practice-3.js
@@ -1,5 +1,22 @@
 'use strict';
 
 function createUpdatedCollection(collectionA, objectB) {
-  return '实现练习要求，并改写该行代码。';
+  var map = collectionA.reduce((m, key) => m.set(key, (m.get(key) || 0) + 1), new Map())
+
+  var result = [];
+
+  for(let item of map){
+    let obj = {};
+    obj["key"] = item[0];
+    obj["count"] = item[1];
+    result.push(obj);
+  }
+
+  for(let val of result) {
+    if(objectB["value"].includes(val["key"])) {
+
+        val["count"] -=  parseInt(val["count"]/3);
+    }
+  }
+  return result;
 }
