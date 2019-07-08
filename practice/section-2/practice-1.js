a0a1f8c9ba4f404a59a32f0904cbd6a5e72f53c2
曾志威 <51374393+DannyZeng2@users.noreply.github.com>
2019-07-08 23:44:20 +0800

    Update practice-1.js

--- practice/section-2/practice-1.js
+++ practice/section-2/practice-1.js
@@ -1,5 +1,14 @@
 'use strict';
 
 function countSameElements(collection) {
-  return '实现练习要求，并改写该行代码。';
+  var map = collection.reduce((m, key) => m.set(key, (m.get(key) || 0) + 1), new Map())
+  var result = [];
+
+  for(let item of map){
+    let obj = {};
+    obj["key"] = item[0];
+    obj["count"] = item[1];
+    result.push(obj);
+  }
+  return result;
 }
